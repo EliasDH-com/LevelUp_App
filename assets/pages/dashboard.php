db3a553c0938f3c4bea0c8816e852c39751ed883
EliasDeHondt <Elias.dehondt@outlook.com>
2023-10-31 21:38:51 +0100

    Removal of wireframes and some CSS

--- assets/pages/dashboard.php
+++ assets/pages/dashboard.php
@@ -1,5 +1,29 @@
 <!DOCTYPE html>
 <!--Van Elias De Hondt-->
+<!--PHP-->
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: /index.php");
+    exit();
+}
+
+$user_id = $_SESSION['user_id'];
+$username = $_SESSION['username'];
+
+$conn = mysqli_connect("localhost", "root", "", "levelup_app_upkeepify");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+            
+// Fetch zones and locations associated with the user
+$zonesQuery = $conn->query("SELECT * FROM zone WHERE zone_id IN (SELECT zone_id FROM user_location WHERE user_id = $user_id)");
+$locationsQuery = $conn->query("SELECT * FROM location WHERE location_id IN (SELECT location_id FROM user_location WHERE user_id = $user_id)");
+$itemsQuery = $conn->query("SELECT * FROM item WHERE item_id IN (SELECT item_id FROM item WHERE assigned = $user_id)");
+?>
+<!--PHP-->
 <!--HTML-->
 <html lang="en">
     <head>
@@ -12,71 +36,58 @@
         <link href="/assets/media/images/favicon.ico" rel="icon">
         <!--Favicon-->
         <!--CSS-->
-        <link rel="stylesheet" href="/assets/css/index-style.css">
+        <link rel="stylesheet" href="/assets/css/dashboard-style.css">
         <!--CSS-->
     </head>
     <body>
+        <header>
+            <div class="logo">
+                <img src="/assets/media/images/index-logo.png" alt="Logo">
+            </div>
+        </header>
         <main>
+            <div class="top-bar">
+                <div class="zones">
+                    <h2>Zones:</h2>
+                    <?php
+                    // Fetch and display zones
+                    while ($zone = $zonesQuery->fetch_assoc()) {
+                        echo '<p>' . $zone['name'] . '</p>';
+                    }
+                    ?>
+                </div>
+                <div class="locations">
+                    <h2>Locations:</h2>
+                    <?php
+                    // Fetch and display locations
+                    while ($location = $locationsQuery->fetch_assoc()) {
+                        echo '<p>' . $location['name'] . '</p>';
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="item-display">
+        <h2>Filtered Items:</h2>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Assigned</th>
+                <th>Zone ID</th>
+            </tr>
             <?php
-            session_start();
-
-            if (!isset($_SESSION['user_id'])) {
-                header("Location: /index.php");
-                exit();
-            }
-
-            $user_id = $_SESSION['user_id'];
-            $username = $_SESSION['username'];
-
-            // Simulated database connection
-            $conn = mysqli_connect("localhost", "root", "", "levelup_app_upkeepify");
-
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            // Fetch zones and locations associated with the user
-            $zonesQuery = $conn->query("SELECT * FROM zone WHERE zone_id IN (SELECT zone_id FROM user_location WHERE user_id = $user_id)");
-            $locationsQuery = $conn->query("SELECT * FROM location WHERE location_id IN (SELECT location_id FROM user_location WHERE user_id = $user_id)");
-            $itemsQuery = $conn->query("SELECT * FROM item WHERE item_id IN (SELECT item_id FROM item WHERE assigned = $user_id)");
-
-            // Display the logo
-            echo '<div class="logo">';
-            // Your logo image here
-            echo '</div>';
-
-            // Display zones and locations
-            echo '<div class="top-bar">';
-            echo '<div class="zones">';
-            echo '<h2>Zones:</h2>';
-            while ($zone = $zonesQuery->fetch_assoc()) {
-                echo '<p>' . $zone['name'] . '</p>';
-            }
-            echo '</div>';
-
-            echo '<div class="locations">';
-            echo '<h2>Locations:</h2>';
-            while ($location = $locationsQuery->fetch_assoc()) {
-                echo '<p>' . $location['name'] . '</p>';
-            }
-            echo '</div>';
-            echo '</div>';
-
-            
-            echo '<div class="item-display">';
-            echo '<h2>Filtered Items:</h2>';
+            // Fetch and display items in a table
             while ($item = $itemsQuery->fetch_assoc()) {
-                echo '<div class="item">';
-                echo '<h3>' . $item['name'] . '</h3>';
-                echo '<p>' . $item['status'] . '</p>';
-                echo '<p>' . $item['assigned'] . '</p>';
-                echo '<p>' . $item['zone_id'] . '</p>';
-                echo '</div>';
+                echo '<tr>';
+                echo '<td>' . $item['name'] . '</td>';
+                echo '<td>' . $item['status'] . '</td>';
+                echo '<td>' . $item['assigned'] . '</td>';
+                echo '<td>' . $item['zone_id'] . '</td>';
+                echo '</tr>';
             }
-            echo '</div>';
-
-            $conn->close();
             ?>
+        </table>
+    </div>
         </main>
     </body>
 </html>
