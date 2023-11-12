2e8e69acefd5a9c2116c2fba7801ab1fe4500bd0
EliasDeHondt <Elias.dehondt@outlook.com>
2023-11-12 15:56:14 +0100

    Updated login screen

---
+++ assets/js/login-animation.js
@@ -0,0 +1,33 @@
+/**
+ * @author Elias De Hondt
+ * @see https://eliasdh.com
+ * @since 31/10/2023
+ */
+
+var design = anime({
+  targets: 'svg #XMLID5',
+  keyframes: [
+    {translateX: -500},
+    {rotateY: 180},
+    {translateX: 920},
+    {rotateY: 0},
+    {translateX: -500},
+    {rotateY: 180},
+    {translateX: -500},
+  ],
+  easing: 'easeInOutSine',
+  duration: 60000,
+});
+
+anime({
+  targets: 'path',
+  translateY: [10, -150],
+  direction: 'alternate',
+  loop: true,
+  delay: function(el, i, l) {
+    return i * 100;
+  },
+  endDelay: function(el, i, l) {
+    return (l - i) * 100;
+  }
+});
