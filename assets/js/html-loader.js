f8ca77622fc0bd2e58066b6de1437968eaa7ac2e
EliasDeHondt <Elias.dehondt@outlook.com>
2023-12-06 11:35:08 +0100

    Remove footer and update bottom section

--- assets/js/html-loader.js
+++ assets/js/html-loader.js
@@ -5,34 +5,24 @@
  */
 
 function loadHTML(myDivId, url) {
-    var xmlhttp;
-    if (window.XMLHttpRequest)
-    {
-        xmlhttp = new XMLHttpRequest();
-    }
-    else
-    {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-    xmlhttp.onreadystatechange = function()
-    {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE )
-        {
-           if(xmlhttp.status == 200){
+    let xmlhttp;
+    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if(xmlhttp.status == 200) {
                document.getElementById(myDivId).innerHTML = xmlhttp.responseText;
-               var allScripts = document.getElementById(myDivId).getElementsByTagName('script-1');
-               for (const element of allScripts)
-               {
-                   eval(element.innerHTML)
+               let scripts = document.getElementById(myDivId).getElementsByTagName('script');
+               for (let i = 0; i < scripts.length; i++) {
+                   let script = document.createElement('script');
+                   script.text = scripts[i].text;
+                   document.body.appendChild(script);
                }
-           }
-           else {
+           } else {
                alert('Error');
            }
         }
     }
-
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
