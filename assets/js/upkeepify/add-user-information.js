e73184585d2b38faeaa380f8f268e48527d16543
EliasDeHondt <Elias.dehondt@outlook.com>
2024-01-04 21:23:48 +0100

    Update response messages in add-item-information.js and add-user-information.js

--- assets/js/upkeepify/add-user-information.js
+++ assets/js/upkeepify/add-user-information.js
@@ -14,12 +14,12 @@ document.getElementById('addUserForm').addEventListener('submit', function(event
     xhr.onload = function() {
         if (xhr.status === 200) {
             var responseMessage = document.getElementById('responseMessage');
-            responseMessage.innerHTML = 'User toegevoegd!';
+            responseMessage.innerHTML = 'User added!';
             responseMessage.classList.add('success');
             responseMessage.style.display = 'block';
         } else {
             var responseMessage = document.getElementById('responseMessage');
-            responseMessage.innerHTML = 'Er is een probleem opgetreden bij het verwerken van het verzoek.';
+            responseMessage.innerHTML = 'There was a problem processing the request.';
             responseMessage.classList.add('error');
             responseMessage.style.display = 'block';
         }
